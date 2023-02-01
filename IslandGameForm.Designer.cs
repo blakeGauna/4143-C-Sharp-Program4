f188c93662e35ac88081b30f5f125caf02d3d360
Blake Gauna <89524476+blakeGauna@users.noreply.github.com>
2023-01-31 19:03:53 -0600

    Add files via upload

---
+++ IslandGameForm.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace program4
+{
+    partial class IslandGameForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.InfoButton = new System.Windows.Forms.Button();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.EnterRowsLabel = new System.Windows.Forms.Label();
+            this.EnterColumnsLabel = new System.Windows.Forms.Label();
+            this.EnterRowsTextBox = new System.Windows.Forms.TextBox();
+            this.EnterColumnsTextBox = new System.Windows.Forms.TextBox();
+            this.MapSizeLabel = new System.Windows.Forms.Label();
+            this.MapLabel = new System.Windows.Forms.Label();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.MapCreateGroupBox = new System.Windows.Forms.GroupBox();
+            this.MapGroupBox = new System.Windows.Forms.GroupBox();
+            this.GuessCountLabel = new System.Windows.Forms.Label();
+            this.GuessGroupBox = new System.Windows.Forms.GroupBox();
+            this.GuessButton = new System.Windows.Forms.Button();
+            this.EnterJTextBox = new System.Windows.Forms.TextBox();
+            this.EnterITextBox = new System.Windows.Forms.TextBox();
+            this.EnterJLabel = new System.Windows.Forms.Label();
+            this.EnterILabel = new System.Windows.Forms.Label();
+            this.HelpGroupBox = new System.Windows.Forms.GroupBox();
+            this.MapCreateGroupBox.SuspendLayout();
+            this.MapGroupBox.SuspendLayout();
+            this.GuessGroupBox.SuspendLayout();
+            this.HelpGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.TitleLabel.Font = new System.Drawing.Font("Courier New", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.TitleLabel.Location = new System.Drawing.Point(201, 9);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(350, 29);
+            this.TitleLabel.TabIndex = 0;
+            this.TitleLabel.Text = "Find the Tropical Island";
+            this.TitleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // InfoButton
+            // 
+            this.InfoButton.Location = new System.Drawing.Point(42, 15);
+            this.InfoButton.Name = "InfoButton";
+            this.InfoButton.Size = new System.Drawing.Size(52, 30);
+            this.InfoButton.TabIndex = 1;
+            this.InfoButton.Text = "HELP";
+            this.InfoButton.UseVisualStyleBackColor = true;
+            this.InfoButton.Click += new System.EventHandler(this.InfoButton_Click);
+            // 
+            // EnterRowsLabel
+            // 
+            this.EnterRowsLabel.AutoSize = true;
+            this.EnterRowsLabel.Location = new System.Drawing.Point(6, 34);
+            this.EnterRowsLabel.Name = "EnterRowsLabel";
+            this.EnterRowsLabel.Size = new System.Drawing.Size(133, 15);
+            this.EnterRowsLabel.TabIndex = 2;
+            this.EnterRowsLabel.Text = "Enter no. of rows:";
+            // 
+            // EnterColumnsLabel
+            // 
+            this.EnterColumnsLabel.AutoSize = true;
+            this.EnterColumnsLabel.Location = new System.Drawing.Point(6, 65);
+            this.EnterColumnsLabel.Name = "EnterColumnsLabel";
+            this.EnterColumnsLabel.Size = new System.Drawing.Size(154, 15);
+            this.EnterColumnsLabel.TabIndex = 3;
+            this.EnterColumnsLabel.Text = "Enter no. of columns:";
+            // 
+            // EnterRowsTextBox
+            // 
+            this.EnterRowsTextBox.Location = new System.Drawing.Point(160, 28);
+            this.EnterRowsTextBox.Name = "EnterRowsTextBox";
+            this.EnterRowsTextBox.Size = new System.Drawing.Size(94, 21);
+            this.EnterRowsTextBox.TabIndex = 4;
+            // 
+            // EnterColumnsTextBox
+            // 
+            this.EnterColumnsTextBox.Location = new System.Drawing.Point(160, 59);
+            this.EnterColumnsTextBox.Name = "EnterColumnsTextBox";
+            this.EnterColumnsTextBox.Size = new System.Drawing.Size(94, 21);
+            this.EnterColumnsTextBox.TabIndex = 5;
+            // 
+            // MapSizeLabel
+            // 
+            this.MapSizeLabel.AutoSize = true;
+            this.MapSizeLabel.Location = new System.Drawing.Point(6, 28);
+            this.MapSizeLabel.Name = "MapSizeLabel";
+            this.MapSizeLabel.Size = new System.Drawing.Size(49, 15);
+            this.MapSizeLabel.TabIndex = 6;
+            this.MapSizeLabel.Text = "Size: ";
+            // 
+            // MapLabel
+            // 
+            this.MapLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.MapLabel.AutoSize = true;
+            this.MapLabel.Location = new System.Drawing.Point(107, 65);
+            this.MapLabel.Name = "MapLabel";
+            this.MapLabel.Size = new System.Drawing.Size(0, 15);
+            this.MapLabel.TabIndex = 7;
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(96, 97);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(70, 25);
+            this.StartButton.TabIndex = 8;
+            this.StartButton.Text = "START";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // MapCreateGroupBox
+            // 
+            this.MapCreateGroupBox.Controls.Add(this.EnterRowsLabel);
+            this.MapCreateGroupBox.Controls.Add(this.StartButton);
+            this.MapCreateGroupBox.Controls.Add(this.EnterColumnsLabel);
+            this.MapCreateGroupBox.Controls.Add(this.EnterRowsTextBox);
+            this.MapCreateGroupBox.Controls.Add(this.EnterColumnsTextBox);
+            this.MapCreateGroupBox.Location = new System.Drawing.Point(12, 73);
+            this.MapCreateGroupBox.Name = "MapCreateGroupBox";
+            this.MapCreateGroupBox.Size = new System.Drawing.Size(271, 139);
+            this.MapCreateGroupBox.TabIndex = 9;
+            this.MapCreateGroupBox.TabStop = false;
+            this.MapCreateGroupBox.Text = "Map Creation";
+            // 
+            // MapGroupBox
+            // 
+            this.MapGroupBox.Controls.Add(this.GuessCountLabel);
+            this.MapGroupBox.Controls.Add(this.MapSizeLabel);
+            this.MapGroupBox.Controls.Add(this.MapLabel);
+            this.MapGroupBox.Location = new System.Drawing.Point(437, 73);
+            this.MapGroupBox.Name = "MapGroupBox";
+            this.MapGroupBox.Size = new System.Drawing.Size(316, 257);
+            this.MapGroupBox.TabIndex = 10;
+            this.MapGroupBox.TabStop = false;
+            this.MapGroupBox.Text = "Map";
+            // 
+            // GuessCountLabel
+            // 
+            this.GuessCountLabel.AutoSize = true;
+            this.GuessCountLabel.Location = new System.Drawing.Point(6, 59);
+            this.GuessCountLabel.Name = "GuessCountLabel";
+            this.GuessCountLabel.Size = new System.Drawing.Size(63, 15);
+            this.GuessCountLabel.TabIndex = 8;
+            this.GuessCountLabel.Text = "Guesses:";
+            // 
+            // GuessGroupBox
+            // 
+            this.GuessGroupBox.Controls.Add(this.GuessButton);
+            this.GuessGroupBox.Controls.Add(this.EnterJTextBox);
+            this.GuessGroupBox.Controls.Add(this.EnterITextBox);
+            this.GuessGroupBox.Controls.Add(this.EnterJLabel);
+            this.GuessGroupBox.Controls.Add(this.EnterILabel);
+            this.GuessGroupBox.Location = new System.Drawing.Point(12, 230);
+            this.GuessGroupBox.Name = "GuessGroupBox";
+            this.GuessGroupBox.Size = new System.Drawing.Size(271, 144);
+            this.GuessGroupBox.TabIndex = 11;
+            this.GuessGroupBox.TabStop = false;
+            this.GuessGroupBox.Text = "Guess";
+            // 
+            // GuessButton
+            // 
+            this.GuessButton.Enabled = false;
+            this.GuessButton.Location = new System.Drawing.Point(91, 103);
+            this.GuessButton.Name = "GuessButton";
+            this.GuessButton.Size = new System.Drawing.Size(75, 23);
+            this.GuessButton.TabIndex = 11;
+            this.GuessButton.Text = "GUESS";
+            this.GuessButton.UseVisualStyleBackColor = true;
+            this.GuessButton.Click += new System.EventHandler(this.GuessButton_Click);
+            // 
+            // EnterJTextBox
+            // 
+            this.EnterJTextBox.Enabled = false;
+            this.EnterJTextBox.Location = new System.Drawing.Point(160, 61);
+            this.EnterJTextBox.Name = "EnterJTextBox";
+            this.EnterJTextBox.Size = new System.Drawing.Size(94, 21);
+            this.EnterJTextBox.TabIndex = 10;
+            // 
+            // EnterITextBox
+            // 
+            this.EnterITextBox.Enabled = false;
+            this.EnterITextBox.Location = new System.Drawing.Point(160, 31);
+            this.EnterITextBox.Name = "EnterITextBox";
+            this.EnterITextBox.Size = new System.Drawing.Size(94, 21);
+            this.EnterITextBox.TabIndex = 9;
+            // 
+            // EnterJLabel
+            // 
+            this.EnterJLabel.AutoSize = true;
+            this.EnterJLabel.Location = new System.Drawing.Point(9, 67);
+            this.EnterJLabel.Name = "EnterJLabel";
+            this.EnterJLabel.Size = new System.Drawing.Size(105, 15);
+            this.EnterJLabel.TabIndex = 1;
+            this.EnterJLabel.Text = "Enter j index:";
+            // 
+            // EnterILabel
+            // 
+            this.EnterILabel.AutoSize = true;
+            this.EnterILabel.Location = new System.Drawing.Point(9, 37);
+            this.EnterILabel.Name = "EnterILabel";
+            this.EnterILabel.Size = new System.Drawing.Size(105, 15);
+            this.EnterILabel.TabIndex = 0;
+            this.EnterILabel.Text = "Enter i index:";
+            // 
+            // HelpGroupBox
+            // 
+            this.HelpGroupBox.Controls.Add(this.InfoButton);
+            this.HelpGroupBox.Location = new System.Drawing.Point(12, 16);
+            this.HelpGroupBox.Name = "HelpGroupBox";
+            this.HelpGroupBox.Size = new System.Drawing.Size(142, 51);
+            this.HelpGroupBox.TabIndex = 12;
+            this.HelpGroupBox.TabStop = false;
+            this.HelpGroupBox.Text = "Need help?";
+            // 
+            // IslandGameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.HelpGroupBox);
+            this.Controls.Add(this.GuessGroupBox);
+            this.Controls.Add(this.MapGroupBox);
+            this.Controls.Add(this.MapCreateGroupBox);
+            this.Controls.Add(this.TitleLabel);
+            this.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Name = "IslandGameForm";
+            this.Text = "Find the Tropical Island";
+            this.MapCreateGroupBox.ResumeLayout(false);
+            this.MapCreateGroupBox.PerformLayout();
+            this.MapGroupBox.ResumeLayout(false);
+            this.MapGroupBox.PerformLayout();
+            this.GuessGroupBox.ResumeLayout(false);
+            this.GuessGroupBox.PerformLayout();
+            this.HelpGroupBox.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label TitleLabel;
+        private Button InfoButton;
+        private HelpProvider helpProvider1;
+        private Label EnterRowsLabel;
+        private Label EnterColumnsLabel;
+        private TextBox EnterRowsTextBox;
+        private TextBox EnterColumnsTextBox;
+        private Label MapSizeLabel;
+        private Label MapLabel;
+        private Button StartButton;
+        private GroupBox MapCreateGroupBox;
+        private GroupBox MapGroupBox;
+        private Label GuessCountLabel;
+        private GroupBox GuessGroupBox;
+        private Label EnterJLabel;
+        private Label EnterILabel;
+        private Button GuessButton;
+        private TextBox EnterJTextBox;
+        private TextBox EnterITextBox;
+        private GroupBox HelpGroupBox;
+    }
+}
---
+++ IslandGameForm.cs
@@ -0,0 +1,227 @@
+/// Angel Badillo, Blake Gauna
+/// 09/27/22
+/// Program 4
+/// This Windows Form App implements an interactive game that allows the user
+/// to create a map within the allowed dimensions of 1x1 up to 10x10. After
+/// the map is create, the user may begin entering guesses until they win the
+/// game. Once the user wins, the user a message box will appear asking the
+/// user to click "Yes" if they want to play again, or click "No" if they want
+/// to quit and close the program.
+namespace program4
+{
+    /// <summary>
+    /// Controls the overall flow of the game and GUI components that make
+    /// up the game.
+    /// </summary>
+    public partial class IslandGameForm : Form
+    {
+        NavigationSystem? navigationSystem;
+        
+        /// <summary>
+        /// Construct the IslandGameForm object and initialize
+        /// navigationSystem reference to null. 
+        /// </summary>
+        public IslandGameForm()
+        {
+            navigationSystem = null;
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Disables "START" button and textboxes in the map creation group.
+        /// </summary>
+        /// <param name="toggle">true enables, false disables.</param>
+        private void ToggleMapCreation(bool toggle)
+        {
+            StartButton.Enabled = toggle;
+            EnterRowsTextBox.Enabled = toggle;
+            EnterColumnsTextBox.Enabled = toggle;
+        }
+
+        /// <summary>
+        /// Disables the "GUESS" button and textboxes in the guess group.
+        /// </summary>
+        /// <param name="toggle">true enables, false disables.</param>
+        private void ToggleGuessing(bool toggle)
+        {
+            GuessButton.Enabled = toggle;
+            EnterITextBox.Enabled = toggle;
+            EnterJTextBox.Enabled = toggle;
+        }
+
+        /// <summary>
+        /// Displays the Victory message box that appears upon winning the
+        /// game. Displays number of guesses user won with, and prompts user
+        /// to either restart or close the game.
+        /// </summary>
+        /// <returns> DialogResult.Yes or DialogResult.No depending 
+        /// on user selection.</returns>
+        private DialogResult ShowWinMessageBox()
+        {
+            return MessageBox.Show("You won with only " +
+                navigationSystem!.GuessCount + " guess(es)!" + 
+                Environment.NewLine + Environment.NewLine +
+                "Would you like to play again?", "Victory!!!",
+                MessageBoxButtons.YesNo);
+        }
+
+        /// <summary>
+        /// Displays the message box that appears when the user enters an
+        /// invalid size during game map creation.
+        /// </summary>
+        private static void ShowInvalidSizeMessageBox()
+        {
+            MessageBox.Show("Please enter valid dimensions for the map."
+                ,"INVALID DIMENSIONS", MessageBoxButtons.OK, 
+                MessageBoxIcon.Error);
+        }
+
+        /// <summary>
+        /// Displays the message box that appears when the user enters an
+        /// invalid guess (not in bounds of map).
+        /// </summary>
+        private static void ShowInvalidGuessMessageBox()
+        {
+            MessageBox.Show("Please enter a valid guess within the bounds " +
+                "of the map.", "INVALID GUESS", MessageBoxButtons.OK, 
+                MessageBoxIcon.Error);
+        }
+
+        /// <summary>
+        /// Displays the Help message box that appears when the user clicks
+        /// the "HELP" button. Provides directions on how to play the game.
+        /// </summary>
+        private static void ShowHelpMessageBox()
+        {
+            MessageBox.Show("To start the game, enter the dimensions of the" +
+                " map." + Environment.NewLine +
+                "The map cannot be 0 x 0 or smaller, nor" +
+                " greater than 10 x 10." +
+                Environment.NewLine + Environment.NewLine +
+                "Once the size is entered and you" +
+                " click the \"START\" button, you can begin entering guesses" +
+                " and submitting your guess with the \"GUESS\" button." +
+                " The indices of the guess must be within the bounds of the" +
+                " size of the map. You may keep entering guesses until the" +
+                " the island has been found. Once it has been found, you" +
+                " will have the option of playing again." +
+                Environment.NewLine + Environment.NewLine +
+                "Legend for map hints:" +
+                Environment.NewLine + "N - Island is North" +
+                Environment.NewLine + "S - Island is South" +
+                Environment.NewLine + "E - Island is East" +
+                Environment.NewLine + "W - Island is West" +
+                Environment.NewLine + "R - Island is in this row" +
+                Environment.NewLine + "C - Island is in this column" +
+                Environment.NewLine + "I - Island has been found",
+                "How to Play Find the Tropical Island",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        /// <summary>
+        /// Handles the event for when "HELP" button is pressed. Displays he
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void InfoButton_Click(object sender, EventArgs e)
+        {
+            ShowHelpMessageBox();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            string tempNumRows;
+            string tempNumCols;
+            int numRows;
+            int numCols;
+            
+            tempNumRows= EnterRowsTextBox.Text;
+            tempNumCols = EnterColumnsTextBox.Text;
+
+            // Check if strings are valid integers
+            if (!(int.TryParse(tempNumRows, out numRows) && 
+                int.TryParse(tempNumCols, out numCols)))
+            {
+                ShowInvalidSizeMessageBox();
+            }
+            // Check if size is invalid
+            else if ((numRows <= 0 || numRows > NavigationSystem.MAXROWS) || 
+                ((numCols <= 0) || (numCols > NavigationSystem.MAXCOLUMNS)))
+            {
+                ShowInvalidSizeMessageBox();
+            }
+            // Set up the game and disable map creation until game ends
+            else
+            {
+                navigationSystem = new NavigationSystem(numRows, numCols);
+                MapSizeLabel.Text = "Size: " + navigationSystem.Size;
+                MapLabel.Text = navigationSystem.ToString();
+                GuessCountLabel.Text = "Guesses: " + 
+                    navigationSystem.GuessCount;
+                
+                // Disable START button and corresponding textboxes
+                ToggleMapCreation(false);
+
+                // Enable GUESS button and corresponding textboxes
+                ToggleGuessing(true);
+            }
+        }
+
+        private void GuessButton_Click(object sender, EventArgs e)
+        {
+            string tempI;
+            string tempJ;
+            int i;
+            int j;
+
+            tempI = EnterITextBox.Text;
+            tempJ = EnterJTextBox.Text;
+            
+            // Check if strings are valid integers
+            if (!(int.TryParse(tempI, out i) && int.TryParse(tempJ, out j)))
+            {
+                ShowInvalidGuessMessageBox();
+            }
+            // Check if guess is invalid
+            else if ((i < 0 || i > navigationSystem!.MaxI) || ((j < 0) || 
+                (j > navigationSystem.MaxJ)))
+            {
+                ShowInvalidGuessMessageBox();
+            }
+            else
+            {
+                bool isWin = navigationSystem.EvaluateGuess(i, j);
+                MapLabel.Text = navigationSystem.ToString();
+                GuessCountLabel.Text = $"Guesses: " +
+                    navigationSystem.GuessCount;
+
+                if(isWin)
+                {
+                    DialogResult dialogResult = ShowWinMessageBox();
+
+                    if(dialogResult == DialogResult.Yes)
+                    {
+                        // Enable START button and corresponding textboxes
+                        ToggleMapCreation(true);
+
+                        // Disable GUESS button and corresponding textboxes
+                        ToggleGuessing(false);
+
+                        // Clear all game data
+                        EnterColumnsTextBox.Text = "";
+                        EnterRowsTextBox.Text = "";
+                        EnterITextBox.Text = "";
+                        EnterJTextBox.Text = "";
+                        MapSizeLabel.Text = "Size:";
+                        GuessCountLabel.Text = "Guesses:";
+                        MapLabel.Text = "";
+                    }
+                    else
+                    {
+                        Close();
+                    }
+                }
+            }
+        }
+    }
+}
